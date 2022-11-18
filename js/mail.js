e3a9e7b9ebe369e363f32804635e6bccaab39868
amarjeetk123 <ak123del@gmail.com>
2022-11-18 22:31:24 +0530

    adding program from day night mode and for text color chnger

---
+++ js/mail.js
@@ -0,0 +1,58 @@
+function SendMSG(e) {
+    e.preventDefault()
+    let nam_txt = document.querySelector("#name")
+    let email_txt = document.querySelector("#email")
+    let subject_txt = document.querySelector("#subject")
+    let message_txt = document.querySelector("#message")
+  
+    let temp = {
+      from_name: nam_txt.value,
+      to_name: "Amarjeet Kumar",
+      email_id : email_txt.value,
+      message : message_txt.value,
+      subject : subject_txt.value,
+    
+    }
+  
+    if (nam_txt.value === "" || email_txt.value === "" || subject_txt.value === "" || message_txt.value === "" ) {
+      return alert("Please Fill Complete Details!!")
+    }
+  
+    emailjs.send('service_s7is795', 'template_b6yq4gz', temp)
+      .then((res) => {
+        if (res.status == 200) {
+          success()
+        } else {
+          failure()
+        }
+      })
+  
+  
+    nam_txt.value = ""
+    email_txt.value = ""
+    subject_txt.value = ""
+    message_txt.value = ""
+  
+  
+  }
+  
+  function success() {
+    let thank = document.querySelector(".msg-pop-up")
+   
+    thank.style.display = "block"
+    setTimeout(() => {
+      thank.style.display = "none"
+    }, 1000)
+  }
+  
+  function failure() {
+    let fail = document.querySelector(".failed-msgpop")
+    fail.style.display = "block"
+    setTimeout(() => {
+      fail.style.display = "none"
+    }, 1000)
+  }
+  
+  // d9edf392-bffd-4fa3-9d98-b2566146073d => token1
+  //template_v4x8ndu
+  // service id = service_f3etz3t
